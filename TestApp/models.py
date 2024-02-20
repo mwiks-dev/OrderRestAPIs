eb7619cda0828c42163f81e75ef2017140382440
Mwikali <mwikali119@gmail.com>
2024-02-20 15:42:17 +0300

    Initialize project and create models for 'Customer' and 'Item' while creating serializers for users

---
+++ TestApp/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length = 10)
+    code = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+class Order(models.Model):
+    item = models.CharField(max_length=10)
+    budget = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.DateTimeField(auto_now_add=True)
